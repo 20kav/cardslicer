c99fdef10c94aaebb44bd9e849380ed006280270
Mdkar <mihirmdkar@gmail.com>
2020-04-14 22:38:27 -0400

    Added way to display slices and large test file

--- cardslicer.py
+++ cardslicer.py
@@ -1,8 +1,8 @@
 import sys
-import pyvista as py
+import pyvista as pv
 
 args = sys.argv
-mesh = py.read(args[1])
+mesh = pv.read(args[1])
 
 #python3 cardslicer.py -filename -starting_slices
 
@@ -10,3 +10,8 @@ n = int(args[2])
 
 slices_x = mesh.slice_along_axis(n=n, axis="x")
 slices_y = mesh.slice_along_axis(n=n, axis="y")
+
+plotter = pv.Plotter()    # instantiate the plotter
+plotter.add_mesh(slices_x)
+plotter.add_mesh(slices_y)    # add a mesh to the scene
+cpos = plotter.show()     # show the rendering window
