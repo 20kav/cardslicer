4ee6abad7e60c1df27bdc6f971fe0af2c60c0e33
Mdkar <mihirmdkar@gmail.com>
2020-04-14 22:56:33 -0400

    Made optional y argument and made function importable

--- cardslicer.py
+++ cardslicer.py
@@ -1,17 +1,23 @@
 import sys
 import pyvista as pv
 
-args = sys.argv
-mesh = pv.read(args[1])
+def cardslice(mesh, nx, ny = None):
+    if ny is None:
+        ny = nx
+    slices_x = mesh.slice_along_axis(n=nx, axis="x")
+    slices_y = mesh.slice_along_axis(n=ny, axis="y")
 
-#python3 cardslicer.py -filename -starting_slices
+    plotter = pv.Plotter()    # instantiate the plotter
+    plotter.add_mesh(slices_x)
+    plotter.add_mesh(slices_y)    # add a mesh to the scene
+    cpos = plotter.show()     # show the rendering window
 
-n = int(args[2])
-
-slices_x = mesh.slice_along_axis(n=n, axis="x")
-slices_y = mesh.slice_along_axis(n=n, axis="y")
-
-plotter = pv.Plotter()    # instantiate the plotter
-plotter.add_mesh(slices_x)
-plotter.add_mesh(slices_y)    # add a mesh to the scene
-cpos = plotter.show()     # show the rendering window
+if __name__ == "__main__":
+    args = sys.argv
+    mesh = pv.read(args[1])
+    #python3 cardslicer.py -filename -starting_slices
+    nx = int(args[2])
+    ny = nx
+    if len(args) > 3:
+        ny = int(args[3])
+    cardslice(mesh, nx, ny)
