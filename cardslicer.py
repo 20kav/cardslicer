6ef8e0f0c7e8e6bbe0e7e88894a658058059ae71
20kav <kavishpsucamp@gmail.com>
2020-04-18 21:39:44 -0400

    Update cardslicer.py
    changed opacity to make liens easier to see, added grids

--- cardslicer.py
+++ cardslicer.py
@@ -2,14 +2,16 @@ import sys
 import pyvista as pv
 import numpy as np
 
-def cardslice(mesh, nx, ny = None):
-    if ny is None:
-        ny = nx
+def cardslice(mesh, nx, ny):
+
     # slices_x = mesh.slice_along_axis(n=nx, axis="x")
     # slices_y = mesh.slice_along_axis(n=ny, axis="y")
     plotter = pv.Plotter()    # instantiate the plotter
-    plotter.add_mesh(mesh)
+    plotter.add_mesh(mesh, opacity = 0.75)
     center = mesh.center
+    plotter.show_grid()
+    plotter.show_axes()
+
     xmin = mesh.bounds[0]
     xmax = mesh.bounds[1]
     ymin = mesh.bounds[2]
@@ -18,24 +20,34 @@ def cardslice(mesh, nx, ny = None):
         orig = (x, mesh.center[1], mesh.center[2])
         slice_x = mesh.slice(origin = orig, normal = 'x')
         if len(slice_x.points) > 0:
-            println("added x-slice")
+            print("added x-slice")
             plotter.add_mesh(slice_x, color='red')
     for y in np.linspace(ymin,ymax,ny):
         orig = (mesh.center[0], y, mesh.center[2])
         slice_y = mesh.slice(origin = orig, normal = 'y')
         if len(slice_y.points) > 0:
-            println("added y-slice")
+            print("added y-slice")
             plotter.add_mesh(slice_y, color='blue')
+    plotter.show(full_screen=False)
+    testplot = pv.Plotter()
+    test_slice = mesh.slice()
+    testplot.add_mesh(test_slice)
+    print(test_slice)
+    print(test_slice.lines)
+    testplot.view_yz()
+    testplot.show()
+    test_slice.save("/Users/Sen/Desktop/lala", binary = False)
 
-    slices_y = mesh.slice(normal = 'y')
 
-    cpos = plotter.show()     # show the rendering window
+
+
+    # show the rendering window
     #pv.save_meshio("mesh.svg", slices_x)
 
 if __name__ == "__main__":
     args = sys.argv
     mesh = pv.read(args[1])
-    #python3 cardslicer.py -filename -starting_slices
+    #python3 cardslicer.py -filename -nx -ny
     nx = int(args[2])
     ny = nx
     if len(args) > 3:
